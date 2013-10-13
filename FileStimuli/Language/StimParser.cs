a8a6fa9e55b38b3134c249232bf347e427ce3024
Morten Engelhardt Olsen <moro.engelhardt@gmail.com>
2013-10-13 18:25:54 +0200

    Add error tagging

--- FileStimuli/Language/StimParser.cs
+++ FileStimuli/Language/StimParser.cs
@@ -39,17 +39,98 @@ namespace Xoriath.FileStimuli.Language
         }
 
 
-        private static readonly Regex mNoArgumentRegex = new Regex(@"[$#][a-zA-Z0-9/\\:]+[ ]*$", RegexOptions.Compiled);
-        private static readonly Regex mOneArgumentRegex = new Regex(@"[$#][a-zA-Z0-9/\\:]+[ ][a-zA-Z0-9/\\:]+[ ]*$", RegexOptions.Compiled);
-        private static readonly Regex mTwoArgumentRegex = new Regex(@"[$#][a-zA-Z0-9/\\:]+[ ][a-zA-Z0-9/\\:]+[ ][a-zA-Z0-9/\\:]+[ ]*$", RegexOptions.Compiled);
+        private static readonly Regex mNoArgumentRegex = new Regex(@"[$#]([a-zA-Z0-9/\\:])*[ ]*$", RegexOptions.Compiled);
+        private static readonly Regex mOneArgumentRegex = new Regex(@"[$#][a-zA-Z0-9/\\:]+[ ]([a-zA-Z0-9/\\:])+[ ]*$", RegexOptions.Compiled);
+        private static readonly Regex mOneArgumentIsNumberRegex = new Regex(@"[#][0-9]+[ ]*$", RegexOptions.Compiled);
+        private static readonly Regex mTwoArgumentRegex = new Regex(@"[$#][a-zA-Z0-9/\\:]+[ ]([a-zA-Z0-9/\\:])+[ ]([a-zA-Z0-9/\\:])+[ ]*$", RegexOptions.Compiled);
+
+        private static readonly string ArgumentNumberErrorType = "Argument number error";
+        private static readonly string UnknownDirectiveErrorType = "Unknown directive";
 
         public static ITagSpan<ErrorTag> ParseError(SnapshotSpan span)
         {
             string text = span.GetText();
 
-            if (text.Contains(@"$stimulate") && !mOneArgumentRegex.IsMatch(text))
-                return new TagSpan<ErrorTag>(span, new ErrorTag("Argument number error"));
-
+            if (text.Contains("//"))
+                return null;
+            else if (text.Contains(@"$stimulate"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$quit"))
+            {
+                if (!mNoArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$break"))
+            {
+                if (!mNoArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$repeat"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$endrep"))
+            {
+                if (!mNoArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$log"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$unlog"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$startlog"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$stoplog"))
+            {
+                if (!mNoArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$fuse"))
+            {
+                if (!mTwoArgumentRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"#"))
+            {
+                if (!mOneArgumentIsNumberRegex.IsMatch(text))
+                    return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                return null;
+            }
+            else if (text.Contains(@"$reset"))
+            {
+                if (!mOneArgumentRegex.IsMatch(text))
+                {
+                    string type = mOneArgumentRegex.Match(text).Groups[1].Value;
+                    if (!(type == "p" || type == "e" || type == "b" || type == "s"))
+                        return new TagSpan<ErrorTag>(span, new ErrorTag(ArgumentNumberErrorType));
+                }
+                return null;
+            }
+            else if (text.Contains(@"$"))
+                return new TagSpan<ErrorTag>(span, new ErrorTag(UnknownDirectiveErrorType));
             return null;
         }
     }
