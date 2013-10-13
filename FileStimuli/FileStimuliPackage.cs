23c46917b612322ce13c420ee4ce091111039eac
Morten Engelhardt Olsen <moro.engelhardt@gmail.com>
2013-10-13 12:03:31 +0200

    Add FileStimuli File template

---
+++ FileStimuli/FileStimuliPackage.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace MortenEngelhardtOlsen.FileStimuli
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    // a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // This attribute is used to register the informations needed to show the this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidFileStimuliPkgString)]
+    [ProvideServiceAttribute(typeof(FileStimuliLanguageService),
+        ServiceName = "FileStimuli Language Service")]
+    [ProvideLanguageServiceAttribute(typeof(FileStimuliLanguageService),
+        "FileStimuli Language Service",
+        106,
+        CodeSense = true,
+        RequestStockColors = true,
+        EnableCommenting = true,
+        EnableAsyncCompletion = true)]
+    [ProvideLanguageExtensionAttribute(typeof(FileStimuliLanguageService),
+        ".stim")]
+
+    public sealed class FileStimuliPackage : Package, IOleComponent
+    {
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public FileStimuliPackage()
+        {
+            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+        }
+        private uint m_componentID;
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();  // required
+
+            // Proffer the service.
+            IServiceContainer serviceContainer = this as IServiceContainer;
+            FileStimuliLanguageService langService = new FileStimuliLanguageService();
+            langService.SetSite(this);
+            serviceContainer.AddService(typeof(FileStimuliLanguageService),
+                                        langService,
+                                        true);
+
+            // Register a timer to call our language service during
+            // idle periods.
+            IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                       as IOleComponentManager;
+            if (m_componentID == 0 && mgr != null)
+            {
+                OLECRINFO[] crinfo = new OLECRINFO[1];
+                crinfo[0].cbSize = (uint)Marshal.SizeOf(typeof(OLECRINFO));
+                crinfo[0].grfcrf = (uint)_OLECRF.olecrfNeedIdleTime |
+                                              (uint)_OLECRF.olecrfNeedPeriodicIdleTime;
+                crinfo[0].grfcadvf = (uint)_OLECADVF.olecadvfModal |
+                                              (uint)_OLECADVF.olecadvfRedrawOff |
+                                              (uint)_OLECADVF.olecadvfWarningsOff;
+                crinfo[0].uIdleTimeInterval = 1000;
+                int hr = mgr.FRegisterComponent(this, crinfo, out m_componentID);
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (m_componentID != 0)
+            {
+                IOleComponentManager mgr = GetService(typeof(SOleComponentManager))
+                                           as IOleComponentManager;
+                if (mgr != null)
+                {
+                    int hr = mgr.FRevokeComponent(m_componentID);
+                }
+                m_componentID = 0;
+            }
+
+            base.Dispose(disposing);
+        }
+
+
+        #region IOleComponent Members
+
+        public int FDoIdle(uint grfidlef)
+        {
+            bool bPeriodic = (grfidlef & (uint)_OLEIDLEF.oleidlefPeriodic) != 0;
+            // Use typeof(TestLanguageService) because we need to
+            // reference the GUID for our language service.
+            FileStimuliLanguageService service = GetService(typeof(FileStimuliLanguageService))
+                                      as FileStimuliLanguageService;
+            if (service != null)
+            {
+                service.OnIdle(bPeriodic);
+            }
+            return 0;
+        }
+
+        public int FContinueMessageLoop(uint uReason,
+                                        IntPtr pvLoopData,
+                                        MSG[] pMsgPeeked)
+        {
+            return 1;
+        }
+
+        public int FPreTranslateMessage(MSG[] pMsg)
+        {
+            return 0;
+        }
+
+        public int FQueryTerminate(int fPromptUser)
+        {
+            return 1;
+        }
+
+        public int FReserved1(uint dwReserved,
+                              uint message,
+                              IntPtr wParam,
+                              IntPtr lParam)
+        {
+            return 1;
+        }
+
+        public IntPtr HwndGetWindow(uint dwWhich, uint dwReserved)
+        {
+            return IntPtr.Zero;
+        }
+
+        public void OnActivationChange(IOleComponent pic,
+                                       int fSameComponent,
+                                       OLECRINFO[] pcrinfo,
+                                       int fHostIsActivating,
+                                       OLECHOSTINFO[] pchostinfo,
+                                       uint dwReserved)
+        {
+        }
+
+        public void OnAppActivate(int fActive, uint dwOtherThreadID)
+        {
+        }
+
+        public void OnEnterState(uint uStateID, int fEnter)
+        {
+        }
+
+        public void OnLoseActivation()
+        {
+        }
+
+        public void Terminate()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ FileStimuli/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MortenEngelhardtOlsen.FileStimuli
+{
+    static class GuidList
+    {
+        public const string guidFileStimuliPkgString = "0e72c057-f129-425e-95a0-172a4b94464b";
+        public const string guidFileStimuliCmdSetString = "bb2b663e-ef85-40be-90b1-e66bf1258e3e";
+
+        public static readonly Guid guidFileStimuliCmdSet = new Guid(guidFileStimuliCmdSetString);
+    };
+}
