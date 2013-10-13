d09c79690e8edecbf9f40ee096767672e3e2b200
Morten Engelhardt Olsen <moro.engelhardt@gmail.com>
2013-10-13 16:52:44 +0200

    Change parser to be more friendly

--- FileStimuli/Language/StimParser.cs
+++ FileStimuli/Language/StimParser.cs
@@ -12,20 +12,29 @@ namespace Xoriath.FileStimuli.Language
         public enum StimLineTypes
         {
             DIRECTIVE,
-            DELAY
+            DELAY,
+            COMMENT
         };
 
         private static readonly Regex mDirectiveRegex = new Regex(@"\$([a-zA-Z0-9]*) .*$", RegexOptions.Compiled);
+        private static readonly Regex mCommentRegex = new Regex(@" *//.*$", RegexOptions.Compiled);
+        private static readonly Regex mDelayRegex = new Regex(@"#[0-9]+.*$", RegexOptions.Compiled);
 
         public static IEnumerable<Tuple<StimLineTypes, SnapshotSpan>> Parse(ITextSnapshotLine line)
         {
             string text = line.GetText();
 
-            if (mDirectiveRegex.Match(text).Success)
+            if (mDirectiveRegex.IsMatch(text))
             {
                 yield return new Tuple<StimLineTypes, SnapshotSpan>(
                     StimLineTypes.DIRECTIVE, new SnapshotSpan(line.Snapshot, line.Start, line.Length));
             }
+            else if (mCommentRegex.IsMatch(text))
+                yield return new Tuple<StimLineTypes, SnapshotSpan>(
+                    StimLineTypes.COMMENT, new SnapshotSpan(line.Snapshot, line.Start, line.Length));
+            else if (mDelayRegex.IsMatch(text))
+                yield return new Tuple<StimLineTypes, SnapshotSpan>(
+                    StimLineTypes.DELAY, new SnapshotSpan(line.Snapshot, line.Start, line.Length));
         }
     }
 }
