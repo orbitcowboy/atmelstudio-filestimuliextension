2c57bfe5fa4350c202c4ae8e53bacb2b89890cc2
Dean Camera <dean@fourwalledcubicle.com>
2013-10-26 22:42:48 +0200

    Move Stim Tagger Provider class into a new file.

--- FileStimuli/Language/StimCodeClassifier.cs
+++ FileStimuli/Language/StimCodeClassifier.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
 
 namespace Xoriath.FileStimuli.Language
 {
--- FileStimuli/Language/StimCodeClassifierProvider.cs
+++ FileStimuli/Language/StimCodeClassifierProvider.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Xoriath.FileStimuli.Language
@@ -19,19 +18,6 @@ namespace Xoriath.FileStimuli.Language
             Func<IClassifier> classifierFunc = () =>
                 new StimCodeClassifier(buffer, mClassificationRegistryService) as IClassifier;
             return buffer.Properties.GetOrCreateSingletonProperty<IClassifier>(classifierFunc);
-        }
-    }
-
-    [Export(typeof(ITaggerProvider))]
-    [TagType(typeof(StimErrorTag))]
-    [ContentType("stim")]
-    public sealed class StimTaggerProvider : ITaggerProvider
-    {
-        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
-        {
-            Func<ITagger<T>> taggerFunc = () => 
-                new StimErrorTagger(buffer) as ITagger<T>;
-            return buffer.Properties.GetOrCreateSingletonProperty<ITagger<T>>(taggerFunc);
         }
     }
 }
